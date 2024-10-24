90b027a7132945a322f4772c1ff9115923771719
ayaz990 <ayazsmemon07@gmail.com>
2024-10-24 18:56:58 +0530

    Hello GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/Navbar";
 import Footer from "./_components/Footer";
-import { GoogleTagManager } from "@next/third-parties/google";
 import SocialLink from "./_components/SocialLink";
 import Script from "next/script";
 
@@ -17,23 +16,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2BW649SZK6" />
-      <Script id="google-analytics">
+      {/* Google Tag Manager Script */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WFF595RC');
+        `}
+      </Script>
+      
+      {/* Google Analytics Script */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2BW649SZK6" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
- 
           gtag('config', 'G-2BW649SZK6');
         `}
       </Script>
+
       <body className={poppins.className}>
         <Navbar />
         {children}
         <Footer />
       </body>
-      <GoogleTagManager gtmId="GTM-WFF595RC"
-      />
     </html>
   );
 }
